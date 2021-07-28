a64334c73fed75fe9d83b74f05f5f9864ddd22d1
Alberto Martinez <alberto@intelligential.tech>
2021-07-28 18:22:52 -0500

    add Prop-Types

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Result = () => {
+const Result = ({ value }) => {
+  console.log('props', value);
   return (
     <div className="result">
-      <span>0</span>
+      <span>{value}</span>
     </div>
   )
+}
+
+Result.propTypes = {
+  value: PropTypes.string.isRequired
+}
+
+Result.defaultProps = {
+  value: "0"
 }
 
 export default Result;
