c8491db6c9f30a8494da6c954f17c97e9dd596f3
Alberto Martinez <alberto@intelligential.tech>
2021-07-31 12:06:18 -0500

    Add components Numbers, Functions and Refactor Button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Result from './components/Result'
-import Button from './components/Button'
+import Numbers from './components/Numbers'
+import Functions from './components/Functions'
 import MathOperations from './components/MathOperations'
 import './App.css';
 
@@ -13,28 +14,10 @@ const App = () => {
   return (
     <main className="react-calculator">
       <Result value={"0"} />
-      <div className="numbers">
-            <Button text="1" clickHandler={(text) => handlerButton(text) } />
-            <button>2</button>
-            <button>3</button>
-            <button>4</button>
-            <button>5</button>
-            <button>6</button>
-            <button>7</button>
-            <button>8</button>
-            <button>9</button>
-            <button>0</button>
-        </div>
-        <div className="functions">
-            <button>
-                clear
-            </button>
-            <button>
-                r
-            </button>
-        </div>
+      <Numbers handlerButton={(text) => handlerButton(text)} />
+      <Functions onClickClear={(text) => handlerButton(text)} onClickDelete={(text) => handlerButton(text)} />
       <MathOperations onClickOperation={(text) => handlerButton(text) } onClickEqual={() => console.log('equals') } />
-   </main>
+    </main>
   );
 }
 
