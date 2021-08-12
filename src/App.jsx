0055eb2990c19f838c58dc71e6bcefcebdbc0ae0
Alberto Martinez <alberto@intelligential.tech>
2021-08-12 18:03:54 -0500

    Add eval function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
+/* eslint no-eval: 0 */
 import React, { useState } from 'react'
+import words from 'lodash.words'
 import Result from './components/Result'
 import Numbers from './components/Numbers'
 import Functions from './components/Functions'
@@ -8,17 +10,20 @@ import './App.css';
 const App = () => {
   console.log('Render App')
   const [result, setResult] = useState('')
-
+  const items = words(result, /[^-^+^*^/]+/g)
+  console.log('items', items)
   return (
     <main className="react-calculator">
-      <Result value={result} />
+      <Result value={items[items.length - 1]} />
       <Numbers handlerButton={(number) => setResult(`${result}${number}`)} />
       <Functions onClickClear={() => setResult('')} 
         onClickDelete={() => {
-          const newResult = result.substring(0, result.length -1);
-          setResult(newResult)
+          if (result.length) {
+            const newResult = result.substring(0, result.length -1);
+            setResult(newResult)
+          }
         }} />
-      <MathOperations onClickOperation={(operation) => setResult(`${result}${operation}`) } onClickEqual={(equal) => setResult(`${result}${equal}`)} />
+      <MathOperations onClickOperation={(operation) => setResult(`${result}${operation}`) } onClickEqual={(equal) => setResult(eval(result).toString()) } />
     </main>
   );
 }
